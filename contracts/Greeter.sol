8d8d72ae5d74f92f2b233969edacc404c2570926
robriks <marsterlund@gmail.com>
2022-05-29 02:00:41 +0000

    implemented two functions, just need to learn how to use arweave to deploy the nft metadata (tokenuri function)

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,19 +5,24 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@rari-capital/solmate/src/tokens/ERC721.sol";
 import "@rari-capital/solmate/src/auth/Owned.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract HotsThots is ERC721("HotsThots", "HOTS"), Owned(msg.sender) {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _idCount;
+
     address[] public hotsFrens;
 
     constructor(address[] memory _whitelist) {
         console.log("Deploying HotsThots membership NFT to whitelisted addresses");
-        setOwner(msg.sender);
         hotsFrens = _whitelist;
+        _mint(msg.sender, _idCount.current());
+        _idCount.increment();
     }
 
-    function addFren(address[] memory _newFrens) public /*onlyOwner()*/ {
-        //hotsFrens.push(_newFrens);
-        //TODO
+    function addFren(address _newFren) public onlyOwner() {
+        hotsFrens.push(_newFren);
     }
 
     function tokenURI(uint256 id) public view override returns (string memory) {
@@ -25,8 +30,14 @@ contract HotsThots is ERC721("HotsThots", "HOTS"), Owned(msg.sender) {
         //TODO W/ ARWEAVE
     }
 
-    function mint() public override {
-        // require(msg.sender to be on whitelist);
-        // TODO 
+    function claim() external {
+        require(balanceOf(msg.sender) == 0, "You already own a HotsThots NFT!");
+        for (uint i; i < hotsFrens.length; i++) {
+            if (hotsFrens[i] == msg.sender) {
+                _mint(msg.sender, _idCount.current());
+                _idCount.increment();
+                return;
+            }
+        }
     }
 }
